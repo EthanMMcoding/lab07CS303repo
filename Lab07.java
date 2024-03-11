35fbce4584a0f26e74bd55d6370c2123fff7a3e8
EthanMMcoding <ethanmartignoni287@gmail.com>
2024-03-11 17:42:17 -0500

    still figuring out the reader

--- Lab07.java
+++ Lab07.java
@@ -1,39 +1,53 @@
 package lab07CS303repo;
 
 import java.io.*;
+import java.util.*;
 
 public class Lab07 {
     
     public static void main(String[] args)throws IOException {
 
-        BinarySearchTree tree = new BinarySearchTree();
-        BinarySearchNode root = new BinarySearchNode(2, "beren");
-        BinarySearchNode node1 = new BinarySearchNode(6, "dior");
-        BinarySearchNode node2 = new BinarySearchNode(4, "elwing");
+        BinarySearchTree CSVTree = new BinarySearchTree(); // tree for the CSV file
+        CSVFileReader fileReader = new CSVFileReader(); // file reader object that will read the csv file into a list of arrays 
 
-        CSVFileReader fileReader = new CSVFileReader(17);
+        fileReader.readFile("UPC_lab07.csv"); // read the csv file into a list of arrays
+        
+        List<String[]> fileList = fileReader.getListOfArrays(); // the list storing the arrays of values from the file
+        // int i = -1;
+        for(String[] e : fileList){ // loop over the file list
+            // BinarySearchNode parentNode = new BinarySearchNode(Integer.parseInt(fileList.get(i)[0]), fileList.get(i)[1]);
+            BinarySearchNode newNode = new BinarySearchNode(Long.parseLong(e[0]), e[1]); // create a new node from each array in the list
+            
+            // i++;
+            // newNode.setParent(newNode);
 
-        // fileReader.readFile("input.dat", 17);
-        // int fileArrLength = fileReader.readFile("input.dat").length;
-        fileReader.printList(fileReader.readFile("input.dat"));
-        // for(int i = 0; i < fileArrLength; i++){
-            // System.out.println(fileReader.readFile("input.dat")[i]);
-            // for(int k = 0; k < fileReader.readFile("input.dat")[i]; k++)
-        // }
-        // for(int i = 0; i < data.size(); i++){
-        //     if(isInteger(data.get(i)));
-        // }
-        // System.out.println((fileReader.readFile("input.dat")[0].getClass()));
+            CSVTree.treeInsert(CSVTree, newNode); // insert the node into the tree
+        }
+        BinarySearchNode CSVTreeRoot = CSVTree.getRoot(); 
+        CSVTree.inOrderTreeWalk(CSVTreeRoot);
+        System.out.println(CSVTree.getRoot());
+        System.out.println(CSVTreeRoot);
+        System.out.println(CSVTreeRoot.getRightChild());
+        System.out.println(CSVTreeRoot.getRightChild().getRightChild());
 
-        BinarySearchTree.treeInsert(tree, root);
-        BinarySearchTree.treeInsert(tree, node1);
-        BinarySearchTree.treeInsert(tree, node2);
-
-        System.out.println(tree.getRoot());
-        System.out.println(root.getRightChild());
-        System.out.println(node1.getLeftChild());
-
-        tree.inOrderTreeWalk(root);
-        System.out.println(tree.iterativeTreeSearch(root, 4));
+        {
+            // BinarySearchTree tree = new BinarySearchTree();
+            // BinarySearchNode root = new BinarySearchNode(2, "beren");
+            // BinarySearchNode node1 = new BinarySearchNode(6, "dior");
+            // BinarySearchNode node2 = new BinarySearchNode(4, "elwing");
+    
+            // tree.treeInsert(tree, root);
+            // tree.treeInsert(tree, node1);
+            // tree.treeInsert(tree, node2);
+    
+    
+            // System.out.println(tree.getRoot());
+            // System.out.println(root.getRightChild());
+            // System.out.println(node1.getLeftChild());
+    
+            // tree.inOrderTreeWalk(root);
+            // System.out.println(tree.iterativeTreeSearch(root, 4));
+        }
+    
     }
 }
